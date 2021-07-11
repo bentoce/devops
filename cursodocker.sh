c5d934c48458cc1a45415f227f7c6911bcda6e50
bentoce <stiforta@gmail.com>
2021-07-11 17:05:41 +0000

    comando salvou no history

--- cursodocker.sh
+++ cursodocker.sh
@@ -7,3 +7,205 @@ docker pull ubuntu
 docker run ubuntu /bin/bash -cNice Content
 docker commit 218a5f36f0af foo-bar
 docker save -o /mnt/shared/foo-bar.tar foo-bar ### SAVANDO IMAGENS DOCKER
+    1  sudo su
+    2  docker ps
+    3  sudo su
+    4  su
+    5  sudo su
+    6  ls -l
+    7  sudo su
+    8  exit
+    9  code .
+   10  sudo su
+   11  ext install ms-vscode-remote.vscode-remote-extensionpack
+   12  sudo su
+   13  firefox &
+   14  sudo su
+   15  firefox &
+   16  code . &
+   17  sudo su
+   18  ls- l
+   19  ls -l
+   20  mkdir docker
+   21  cd docker/
+   22  gedit .
+   23  gedit docker.sh &
+   24  ps -as 
+   25  cd docker/
+   26  ls -l
+   27  docker ps 
+   28  ls -l
+   29  cd docker/
+   30  git init
+   31  cd /var
+   32  ls -l
+   33  docker run -it debain debian bash
+   34  ls -l
+   35  rm -rf docker/
+   36  ls -l
+   37  cd /var/
+   38  git help
+   39  ls -l
+   40  mkdir respositorio1
+   41  cd 
+   42  cd /home/bento/
+   43  ls -l
+   44  mkdir devops
+   45  cd devops/
+   46  git init
+   47  ls -la
+   48  ls -l
+   49  cd .git/
+   50  ls -l
+   51  cd ../
+   52  ls -l
+   53  git help status
+   54  git status
+   55  git help config
+   56  git status
+   57  git config --local user.name "bentoce"
+   58  git config --local user.email "stiforta@gmail.com"
+   59  echo "git config --local user.name "bentoce"" >> gitconfig.sh
+   60  echo "git config --local user.email "stiforta@gmail.com"" >> gitconfig.sh
+   61  vim gitconfig.sh 
+   62  ls -l
+   63  git add .
+   64  git commit -m " Arquivo com os camandos git"
+   65  ls -l
+   66  git status
+   67  mkdir java ruby javascript
+   68  ls -l
+   69  git status
+   70  git add .
+   71  git status
+   72  git logs
+   73  git log
+   74  ls -l
+   75  git add .
+   76  ls -l
+   77  git status
+   78  cd /opt
+   79  ls -l
+   80  sudo su
+   81  cd /home/bento/devops/
+   82  ls -l
+   83  git commit -m "adiçao dos diretorios dos curso de programação"
+   84  git log
+   85  git -v log
+   86  git log -v
+   87  git log --online
+   88  git log --oneline
+   89  git config -l
+   90  ls -l
+   91  mkdir html&css
+   92  mkdir frontend
+   93  mkdir ls -l
+   94  ls -l
+   95  LS -L
+   96  ls -l
+   97  git status
+   98  ls -l
+   99  touch .gitignore
+  100  git status
+  101  git add .gitignore 
+  102  commit -m "Adicionado gitingnore"
+  103  git commit -m "Adicionado gitingnore"
+  104  git status
+  105  git log
+  106  touch faq1.txt faq2.txt
+  107  vim .gitignore 
+  108  git status
+  109  git commit -m "Arquivo faqs inginorado"
+  110  git add
+  111  git add .
+  112  git commit -m "Arquivo faqs inginorado"
+  113  ls -l
+  114  exit
+  115  sudo su
+  116  cd /home/bento/devops/
+  117  git branch
+  118  cd devops/
+  119  git remote -v
+  120  ls -l
+  121  LS -L
+  122  ls -l
+  123  git remote add origin https://github.com/bentoce/devops
+  124  git remote -v
+  125  git push origin master
+  126  ls -l
+  127  cat "git remote add origin https://github.com/bentoce/devops" >> gitconfig.sh 
+  128  echo "git remote add origin https://github.com/bentoce/devops" >> gitconfig.sh 
+  129  echo "git remote -v" >> gitconfig.sh 
+  130  git status
+  131  git commit -a
+  132  git add .
+  133  git status
+  134  git commit -m "mais comando o arquivo gitconfig.sh"
+  135  git push origin master
+  136  git remote -v
+  137  vim gitconfig.sh 
+  138  ssh-keygen 
+  139  vim /home/bento/.ssh/id_rsa.pub 
+  140  git remote add origin https://github.com/bentoti/bootcamps.git
+  141  git push -u origin main
+  142  git remote add origin https://github.com/bentoti/bootcamps.git
+  143  git branch -M main
+  144  git push -u origin main
+  145  git push origin master
+  146  git push -u origin main
+  147  git remote -v
+  148  cd devops/
+  149  git remote -v
+  150  git push local master
+  151  sudo su
+  152  git branch
+  153  git branch devops
+  154  git branch
+  155  ls- l
+  156  ls -l
+  157  cd devops/
+  158  ls -l
+  159  vim cursodocker.sh
+  160  ls -l
+  161  git push origin master
+  162  git push origin 
+  163  git add .
+  164  git commit -m "Arquivo docker"
+  165  git push origin master
+  166  git push origin 
+  167  git blanch master
+  168  git blanch 
+  169  git blanch
+  170  git
+  171  git branch
+  172  git push origin main
+  173  sudo su
+  174  git status
+  175  git commit -m "ADICAO DECMD"
+  176  git add
+  177  git add.
+  178  git add .
+  179  git commit -m "ADICAO DECMD"
+  180  git push origin main
+  181  docker ps
+  182  docker run -it ubuntu:19.04 bash
+  183  ls -l
+  184  docker run -d centos:7 /bin/bash -c "ping 8.8.8.8"
+  185  docker ps 
+  186  docker inspech
+  187  docker inspect 
+  188  docker ps
+  189  docker commit 7d6ac91869cc centos7
+  190  ls -l
+  191  docker images
+  192  docker run -it centos7 bash
+  193  docker ps
+  194  docker inspact centos:7
+  195  docker --help
+  196  docker inspect 7d6ac91869cc
+  197  docker logs
+  198  docker logs 7d6ac91869cce5306e6d35c7868082809deca27373ce91fcda21d85135e2a587
+  199  docker attach 7d6ac91869cce5306e6d35c7868082809deca27373ce91fcda21d85135e2a587
+  200  docker ps
+  201  docker ps -a
+  202  history >> cursodocker.sh 
